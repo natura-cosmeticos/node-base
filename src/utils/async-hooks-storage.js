62abe8e62a38c61f8326e62a21525fec7657b7b8
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-08-05 22:03:00 -0300

    Updated async hooks storage module

--- src/utils/async-hooks-storage.js
+++ src/utils/async-hooks-storage.js
@@ -1,4 +1,5 @@
 const uuidV4 = require('uuid/v4');
+const fs = require('fs');
 const asyncHooks = require('async_hooks');
 
 // Main storage object
@@ -7,12 +8,43 @@ const asyncHooksStorage = { entries: {} };
 // Async Hooks list
 const asyncHooksList = {};
 
+// Log flag
+let hasLog = false;
+
+// Graph flag
+let hasGraph = false;
+
+// Graph persistence flag
+let graphPersistence = false;
+
+// Debug graph
+const graph = [];
+
 // Async Hooks Entry class with automatic UUIDV4 ids
 class AsyncHooksEntry {
   constructor(type) {
     this.id = uuidV4();
     this.type = type;
     this.context = new Map();
+  }
+}
+
+// eslint-disable-next-line
+function syncLog(logStr, graphStr, asyncId, remove) {
+  if (hasLog) {
+    fs.writeSync(1, `${logStr}\n`);
+  }
+  if (hasGraph) {
+    if (!graphPersistence && remove) {
+      delete graph[asyncId];
+    } else {
+      // eslint-disable-next-line no-lonely-if
+      if (graph[asyncId]) {
+        graph[asyncId] += graphStr;
+      } else {
+        graph[asyncId] = graphStr;
+      }
+    }
   }
 }
 
@@ -24,9 +56,10 @@ function existAsyncHooksEntry(asyncId) {
 // Initialize a new Async Hook Resource
 function init(asyncId, type, triggerAsyncId) {
   if (existAsyncHooksEntry(triggerAsyncId)) {
-    // Attach the asyncId context with the parent context
+    // Attach the asyncId entry with the parent entry
     asyncHooksStorage.entries[asyncId] = asyncHooksStorage.entries[triggerAsyncId];
   }
+  syncLog(`init           ${asyncId}\t${triggerAsyncId}\t${type}`, `[${triggerAsyncId}] ${type} INIT `, asyncId, false);
 }
 
 // When an asynchronous operation is initiated or completes a callback is called to
@@ -36,6 +69,7 @@ function before(asyncId) {
     asyncHooksList[asyncId] = asyncHooksStorage.currentEntry;
     asyncHooksStorage.currentEntry = asyncHooksStorage.entries[asyncId];
   }
+  syncLog(`before         ${asyncId}`, 'BEFORE ', asyncId, false);
 }
 
 // Called immediately after the callback specified in before is completed
@@ -43,20 +77,32 @@ function after(asyncId) {
   if (existAsyncHooksEntry(asyncId)) {
     asyncHooksStorage.currentEntry = asyncHooksList[asyncId];
   }
+  syncLog(`after          ${asyncId}`, 'AFTER ', asyncId, false);
 }
 
 // Called after the resource corresponding to asyncId is destroyed
 function destroy(asyncId) {
   if (existAsyncHooksEntry(asyncId)) {
-    delete asyncHooksList[asyncId];
     delete asyncHooksStorage.entries[asyncId];
+    delete asyncHooksList[asyncId];
   }
+  syncLog(`destroy        ${asyncId}`, 'DESTROY ', asyncId, true);
+}
+
+// Called when the resolve function passed to the Promise constructor is invoked
+function promiseResolve(asyncId) {
+  syncLog(`promiseResolve ${asyncId}`, 'PROMISERESOLVE ', asyncId, false);
 }
 
 // Enable the callbacks for a given AsyncHook instance
-asyncHooksStorage.enable = () => asyncHooks.createHook({
-  after, before, destroy, init,
-}).enable();
+asyncHooksStorage.enable = (bLog = false, bGraph = false, bPersistence = false) => {
+  hasLog = bLog;
+  hasGraph = bGraph;
+  graphPersistence = bPersistence;
+  asyncHooks.createHook({
+    after, before, destroy, init, promiseResolve,
+  }).enable();
+};
 
 // Create new Entry into the asyncHooksStorage and link with the currentEntry property
 asyncHooksStorage.newEntry = (type) => {
@@ -67,10 +113,9 @@ asyncHooksStorage.newEntry = (type) => {
 };
 
 // Get an asyncHookStorage entry or return undefined
-asyncHooksStorage.getEntry = key => (
-  (asyncHooksStorage.currentEntry && asyncHooksStorage.currentEntry.context)
-    ? asyncHooksStorage.currentEntry.context.get(key)
-    : undefined);
+asyncHooksStorage.getEntry = key => ((asyncHooksStorage.currentEntry
+  && asyncHooksStorage.currentEntry.context)
+  ? asyncHooksStorage.currentEntry.context.get(key) : undefined);
 
 // Set a new asyncHookStorage entry
 asyncHooksStorage.setEntry = (key, value) => {
@@ -81,6 +126,16 @@ asyncHooksStorage.setEntry = (key, value) => {
   }
 
   return false;
+};
+
+// Get execution graph
+asyncHooksStorage.getGraph = () => graph;
+
+// Print graph
+asyncHooksStorage.showGraph = () => {
+  for (let counter = 0; counter < graph.length; counter += 1) {
+    if (graph[counter]) fs.writeSync(1, `[${counter}] ${graph[counter]}\n`);
+  }
 };
 
 module.exports = asyncHooksStorage;
